312c0c547487ee1ced2cd90a37996664de3e03bc
acai-alicia <128638366+acai-alicia@users.noreply.github.com>
2023-03-22 15:36:57 -0400

    Update and rename lab6.py to grant-a-lab6.py

--- grant-a-lab6.py
+++ grant-a-lab6.py
@@ -1,34 +1,37 @@
-def menu():
-    print("""Menu
--------------
-1. Encode
-2. Decode
-3. Exit
-    """)
+def menu():  # menu formatted
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Exit\n")
 
 
 def encoder(psswrd):
-    encoded = ""
+    encoded = ""  # empty string
+    # for each digit in the inputted password, we add 3 to the value
     for digit in psswrd:
         encoded += str((int(digit) + 3) % 10)
     return encoded
 
 
 def decoder(psswrd):
-    original = ""
+    original = ""  # empty string
+    # for each digit in the inputted password, we subtract 3 from the value
     for digit in psswrd:
         original += str((int(digit) - 3) % 10)
     return original
 
 
 def main():
+    
+    # while loop to keep main going until user breaks (option 3)
     while True:
-        menu()
+        menu()  # print menu
         option = int(input("Please enter an option: "))
 
         if option == 1:
             psswrd = (input("Please enter your password to encode: "))
-            ans = encoder(psswrd)
+            ans = encoder(psswrd)  # assign to variable "ans" to refer to it in option 2
             print("Your password has been encoded and stored!\n")
 
         elif option == 2:
